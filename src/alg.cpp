cac45a0ba943dd3ba14c645f32357cb86f79e1a9
TiunovaYana <99291404+TiunovaYana@users.noreply.github.com>
2022-03-27 19:42:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,42 +12,42 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int kolvo = 0;
-    int end = len - 1;
+    int end2 = len - 1;
     for (int i = len; i > 0; i--) {
-        if (arr[end] > value) {
-            end--;
+        if (arr[end2] > value) {
+            end2--;
         }
     }
-    for (int begin = 0; begin < end; begin++) {
-        for (int j = end; j > begin; j--) {
-            if (arr[begin] + arr[j] == value) {
+    for (int begin2 = 0; begin2 < end2; begin2++) {
+        for (int j = end2; j > begin2; j--) {
+            if (arr[begin2] + arr[j] == value) {
                 kolvo++;
             }
         }
-        if (arr[begin] != arr[begin + 1]) {
-            end--;
+        if (arr[begin2] != arr[begin2 + 1]) {
+            end2--;
         }
     }
     return kolvo;
 }
 int countPairs3(int *arr, int len, int value) {
     int kolvo = 0;
-    int middle = 0, 
-    int begin, end;
+    int middle = 0;
+    int begin3, end3;
     for (int i = 0; i < len; i++) {
-        begin = i + 1;
-        end = len;
-        while (begin < end) {
-            middle = (begin + end) / 2;
+        begin3 = i + 1;
+        end3 = len;
+        while (begin3 < end3) {
+            middle = (begin3 + end3) / 2;
             if (arr[middle] < (value - arr[i])) {
-                begin = ++middle;
+                begin3 = ++middle;
             } else {
-                end = middle;
+                end3 = middle;
             }
         }
-        while (arr[begin] == (value - arr[i])) {
+        while (arr[begin3] == (value - arr[i])) {
             kolvo++;
-            begin++;
+            begin3++;
         }
     }
     return kolvo;
